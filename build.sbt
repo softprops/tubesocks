daf1939cf6ba58ba27cb01997d32fa727d8c90c7
softprops <d.tangren@gmail.com>
2013-08-25 22:14:42 -0400

    break things up

--- build.sbt
+++ build.sbt
@@ -7,7 +7,7 @@ version := "0.1.1-SNAPSHOT"
 description := "A comfortable and fashionable way to have bi-directional conversations with modern web servers"
 
 libraryDependencies ++= Seq(
-  "com.ning" % "async-http-client" % "1.7.9",
+  "com.ning" % "async-http-client" % "1.7.19",
   "net.databinder" %% "unfiltered-netty-websockets" % "0.6.4" % "test",
   "net.databinder" %% "unfiltered-spec" % "0.6.4" % "test",
   "org.slf4j" % "slf4j-jdk14" % "1.6.2")
@@ -25,7 +25,9 @@ buildInfoKeys := Seq[BuildInfoKey](version)
 buildInfoPackage := "tubesocks"
 
 crossScalaVersions ++= Seq(
-  "2.8.1", "2.8.2", "2.9.0-1", "2.9.1", "2.9.1-1", "2.9.2")
+  "2.8.1", "2.8.2",
+  "2.9.0-1", "2.9.1", "2.9.1-1", "2.9.2", "2.9.3",
+  "2.10.0", "2.10.1")
 
 publishTo := Some(Opts.resolver.sonatypeStaging)
 
--- src/main/scala/tubesocks.scala
+++ src/main/scala/tubesocks.scala
@@ -5,34 +5,7 @@ import com.ning.http.client.websocket.{
   WebSocket, DefaultWebSocketListener, WebSocketUpgradeHandler }
 import java.net.URI
 
-// we are using the netty client
-// the grizly client implements streaming
-// consider this...
-trait Socket {
-  def send(s: String): Unit
-  def open: Boolean
-  def close: Unit
-}
-
-class DefaultSocket(underlying: WebSocket) extends Socket {
-  def send(s: String) =
-    if (underlying.isOpen) underlying.sendTextMessage(s)
-    else ()
-  def open = underlying.isOpen
-  def close = if (underlying.isOpen) underlying.close else ()
-  override def toString() = "%s(%s)" format(
-    getClass().getName, if(open) "open" else "closed")
-}
-
-sealed trait Event
-case class Message(text: String, socket: Socket) extends Event
-case class Open(socket: Socket) extends Event
-case class Close(socket: Socket) extends Event 
-case class Error(exception: Throwable) extends Event
-case class Fragment(text: String) extends Event
-case class EOF(text: String) extends Event
-
-/** A builder of sorts for Sockets */
+/** A builder of sorts for (Web)Sockets */
 object Sock {
   /** A partial function signature for handing Socket events */
   type Handler = PartialFunction[Event, Any]
@@ -41,7 +14,26 @@ object Sock {
     lazy val discard: Handler = {
       case e: Event => ()
     }
-    def apply(pf: Handler) = {
+
+    case class ReconnectingListen(times: Int, pausing: Int) {
+      def apply(pf: Handler) = {
+        def complete(e: Event) = (pf orElse discard)(e)
+        // note: we would just use the TextListener below BUT
+        // it's very convenient to make #onMessage(m) respondable
+        new DefaultWebSocketListener {
+          override def onMessage(m: String) = complete(Message(m, new DefaultSocket(this.webSocket)))
+          override def onOpen(ws: WebSocket) = complete(Open(new DefaultSocket(ws)))
+          override def onClose(ws: WebSocket) = complete(Close(new DefaultSocket(ws)))
+          override def onError(t: Throwable) = complete(Error(t))
+          override def onFragment(fragment: String, last: Boolean) =
+            complete(if (last) EOF(fragment) else Fragment(fragment))
+        }
+      }
+    }
+
+    def reconnecting(times: Int, pausing: Int) = ReconnectingListen(time, pausing)
+
+    def apply(pf: Handler)  = {
       def complete(e: Event) = (pf orElse discard)(e)
       // note: we would just use the TextListener below BUT
       // it's very convenient to make #onMessage(m) respondable
@@ -56,6 +48,9 @@ object Sock {
     }
   }
 
+  def reconnecting(times: Int = -1, pausing: Int = 0)(uri: URI)(f: Handler): Socket =
+    configure(identity)(times, pausing)(uri)(f)
+
   /** URI factory for returning a websocket
    *  @param str string uri
    *  @return a function that takes a Handler and returns a Socket */
@@ -66,18 +61,19 @@ object Sock {
    *  @param uri websocket endpoint
    *  @param f Handler function */
   def apply(uri: URI)(f: Handler): Socket =
-    configure(identity)(uri)(f)
+    configure(identity)()(uri)(f)
 
   /** Provides a means of customizing client configuration
    *  @param conf configuration building function
    *  @param uri websocket endpoint
    *  @param f Handler function */
   def configure(conf: AsyncHttpClientConfig.Builder => AsyncHttpClientConfig.Builder)
+               (reconnectAttempts: Int = 0, pausing: Int = 0)
                (uri: URI)(f: Handler): Socket =
     new DefaultSocket(mkClient(conf(defaultConfig))
                       .prepareGet(uri.toString)
                       .execute(new WebSocketUpgradeHandler.Builder()
-                               .addWebSocketListener(Listen(f))
+                               .addWebSocketListener(Listen.reconnecting(reconnectAttempts, pausing)(f))
                       .build())
                       .get())
 
