3b43bb29ef1c2cca3d5ff7c6b71bee4e9b6574be
softprops <d.tangren@gmail.com>
2014-07-10 13:58:48 -0400

    we are on 2.10 now so we can use promises

--- src/test/scala/spec.scala
+++ src/test/scala/spec.scala
@@ -1,34 +1,35 @@
 package tubesocks
 
-import org.specs2.mutable._
+import unfiltered.netty.websockets.Planify
+import unfiltered.netty.websockets.{ Message => UFMessage, Text }
+import org.specs2.mutable.Specification
+import scala.concurrent.Promise
+import scala.concurrent.duration._
 
 object TubesocksSpec extends Specification
   with unfiltered.specs2.netty.Served {
 
-  import unfiltered.netty.websockets.Planify
-  import unfiltered.netty.websockets.{ Message, Text }
-
   def setup =
     _.handler(Planify({
       case _ => {
-          case Message(s, Text(msg)) => s.send(msg)
+          case UFMessage(s, Text(msg)) => s.send(msg)
         }
     }))
 
   "Socks" should {
     "receive messages" in {
-      import java.util.concurrent.{ CountDownLatch, TimeUnit }
-      var m = scala.collection.mutable.Map.empty[String, String]
-      val l = new CountDownLatch(1)
+      val promise = Promise[String]()
       Sock.uri(host.to_uri.toString.replace("http", "ws")) {
         case tubesocks.Open(s) =>
           s.send("i'm open")
         case tubesocks.Message(t, _) =>
-          m += ("rec" -> t)
-          l.countDown
+          if (!promise.isCompleted) {
+            promise.success(t)
+          }
       }
-      l.await(2, TimeUnit.MILLISECONDS)
-      m must havePair(("rec", "i'm open"))
+      promise.future must be_==("i'm open").await(
+        retries = 5, Duration(2, MILLISECONDS)
+      )
     }
   }
 }
